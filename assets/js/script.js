dc9e67ad97ad3454eeee9a9cad8c8ffded380755
gurbanli97 <ali.e.gurbanli@gmail.com>
2021-04-14 19:42:35 +0400

    unauth

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,10 @@ var inputValue1 = input.value
 
 
 
-
-
-
 input.oninput = function () {
     if(inputValue1 !== null) {
         document.getElementById('btn-disable').classList.remove('disabled')
         document.getElementById('btn-disable').classList.add('active')
-
-    } 
+    }
 }
 
